ba5fd5b11543a84d4916ff6daf59ec4b32d25ce3
moritzWa <wallawitsch@gmail.com>
2025-04-10 20:48:58 -0700

    ignore sidebar

--- client/src/components/AppLayout.tsx
+++ client/src/components/AppLayout.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from 'react';
-import { AppSidebar } from './AppSidebar';
-import { SidebarInset } from './ui/sidebar';
 import { Toaster } from './ui/toaster';
 
 interface AppLayoutProps {
@@ -10,11 +8,11 @@ interface AppLayoutProps {
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex h-screen w-full">
-      <AppSidebar />
-      <SidebarInset>
-        <div className="flex-1 overflow-auto">{children}</div>
-        <Toaster />
-      </SidebarInset>
+      {/* <AppSidebar />
+      <SidebarInset> */}
+      <div className="flex-1 overflow-auto">{children}</div>
+      <Toaster />
+      {/* </SidebarInset> */}
     </div>
   );
 }
